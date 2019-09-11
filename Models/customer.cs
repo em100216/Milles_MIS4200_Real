a1d6efeb91fb3ac38a00426568a0bb7a2936c3f6
em100216 <em100216@ohio.edu>
2019-09-11 11:39:56 -0400

    commit classes

---
+++ Models/customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Milles_MIS4200_Real.Models
+{
+    public class customer
+    {
+        public int customerId { get; set; }
+        public string customerFirstName { get; set; }
+        public string customerLastName { get; set; }
+        public string phone { get; set; }
+        public int email { get; set; }
+        public int MyProperty { get; set; }
+        // prop tab tab 
+        public ICollection<order> orders { get; set; }
+    }
+}
